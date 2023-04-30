dbabea81e4e2a15ce1775632fa433869e3551663
m-foroutan <m.foroutan26@yahoo.com>
2023-05-01 00:02:39 +0330

    post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
 
 @Module({
@@ -11,6 +12,7 @@ import { UserModule } from './user/user.module';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_URL),
     UserModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService],
